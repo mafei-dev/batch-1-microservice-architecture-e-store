c1d00d26a6561001ef67f8ba35ccde9aeae517ea
mafei <mafei.dev@gmail.com>
2023-03-08 19:24:31 +0530

    add inits

---
+++ user-service/src/main/java/com/example/userservice/UserServiceApplication.java
@@ -0,0 +1,22 @@
+package com.example.userservice;
+
+import lombok.AllArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@AllArgsConstructor
+@EnableScheduling
+public class UserServiceApplication implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
